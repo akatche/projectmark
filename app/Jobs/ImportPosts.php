ab7facc003f9fab2c25a213daf74e071735778dd
Alejandro Katcheroff <akatcheroff@gmail.com>
2020-09-18 11:04:26 -0300

    Fix in import posts

--- app/Jobs/ImportPosts.php
+++ app/Jobs/ImportPosts.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 
@@ -36,5 +37,8 @@ class ImportPosts implements ShouldQueue
                 'user_id' => $admin->id
             ]);
         });
+
+        Cache::flush();
+
     }
 }
