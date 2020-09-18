c4c2f2f168d47521e20bc1e79fd775a415859a4b
Alejandro Katcheroff <akatcheroff@gmail.com>
2020-09-17 22:33:57 -0300

    Added custom logo

--- resources/js/Layouts/HomePageLayout.vue
+++ resources/js/Layouts/HomePageLayout.vue
@@ -8,7 +8,7 @@
                         <!-- Logo -->
                         <div class="flex-shrink-0 flex items-center">
                             <a href="/">
-                                <jet-application-mark class="block h-9 w-auto" />
+                                <jet-application-mark class="h-20" />
                             </a>
                         </div>
 
---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+    <img class="h-40 w-auto" src="/img/logo_transparent.png" alt="Logo">
+</a>
